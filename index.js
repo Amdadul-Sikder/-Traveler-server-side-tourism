a633903efe4789033717e905b34621e2ac1ca245
Amdadul Sikder <amdadulsikder3@gmail.com>
2021-10-30 22:36:12 +0600

    services data added to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nocvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,7 +18,16 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("database connected");
+        // console.log("database connected");
+        const database = client.db("traveler");
+        const servicesCollection = database.collection('services');
+
+        //GET Products API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
     }
     finally {
         // await client.close()
